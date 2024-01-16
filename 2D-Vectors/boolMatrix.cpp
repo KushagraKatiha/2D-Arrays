28c24078f7ce0b045c93f459d5f27c22c92e88ba
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-01-16 22:57:04 +0530

    most ones row type3

--- 2D-Vectors/boolMatrix.cpp
+++ 2D-Vectors/boolMatrix.cpp
@@ -2,6 +2,29 @@
 #include<vector>
 using namespace std;
 
+int maximum_1_row_type3(vector<vector<int>> &v, int rows, int cols){
+    // Check which row will have the 1 at leftmost index
+    int row_num = -1;
+    int i, j;
+    i = 0;
+    j = cols-1;
+
+    while(j >= 0 && v[0][j]){
+        if(v[i][j] == 1){
+            row_num = 0;
+            j--;
+        }
+    }
+
+    for(int i = 1; i < rows; i++){
+        while(j >= 0 && v[i][j] == 1){
+            j--;
+            row_num = i;
+        }
+    }
+    return row_num;
+}
+
 int maximum_1_row_type2(vector<vector<int>> &v, int row, int col){
     // Checking which array will have the 1 at leftmost index
     
@@ -66,8 +89,9 @@ int main(){
     }
 
     // int row_num = maximum_1_row(v, row, col);
-    int num_of_ones = maximum_1_row_type2(v, row, col);
+    // int row_num = maximum_1_row_type2(v, row, col);
+    int row_num = maximum_1_row_type3(v, row, col);
 
-    cout<<"Row with maximum 1(s) is => "<<num_of_ones<<endl;
+    cout<<"Row with maximum 1(s) is => "<<row_num<<endl;
     return 0;
 }
