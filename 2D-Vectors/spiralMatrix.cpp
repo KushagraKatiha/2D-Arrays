764f1fde14aee5be82c03205345a863e0c3e77af
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-01-17 20:23:48 +0530

    printing spiral matrix

--- 2D-Vectors/spiralMatrix.cpp
+++ 2D-Vectors/spiralMatrix.cpp
@@ -4,7 +4,37 @@ using namespace std;
 
 // Function to print matrix in spiral order
 void printSpiral(vector<vector<int>> v, int row, int col){
-    
+    int direction = 0;
+    int left = 0;
+    int right = col-1;
+    int top = 0;
+    int bottom = row-1;
+
+    while(left <= right && top <= bottom){
+        
+        if(direction == 0){
+            for(int i = left; i <= right; i++){
+                cout<<v[top][i]<<" ";
+            }
+            top++;
+        }else if(direction == 1){
+            for(int i = top; i <= bottom; i++){
+                cout<<v[i][right]<<" ";
+            }
+            right--;
+        }else if(direction == 2){
+            for(int i = right; i >= left; i--){
+                cout<<v[bottom][i]<<" ";
+            }
+            bottom--;
+        }else if(direction == 3){
+            for(int i = bottom; i >= top; i--){
+                cout<<v[i][left]<<" ";
+            }
+            left++;
+        }
+        direction = (direction+1) % 4;
+    }
 }
 
 int main(){
